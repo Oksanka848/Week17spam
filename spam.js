e920558fcab18d45c7c887bf9eee77dd8bc2f665
Oksanaka848 <okmitka@mail.ru>
2022-11-05 21:57:10 +0300

    ch

--- spam.js
+++ spam.js
@@ -16,11 +16,28 @@ button.addEventListener('click', () => {
 });
 
 function createElement(ci) {
-  const newComment = document.createElement('div');
+  let newComment = document.createElement('div');
   for (let i=0; i<newComment.length; i++) {
-    newComment[i].style.marginLeft = i + '450px' ;}
-  newComment.setAttribute('data-comment-id', comments.id);
+    newComment[i].style.marginLeft = i + '15px';}
+  
   newComment.textContent = ci;
   commentList.appendChild(newComment);
 }
-
+function checkSpam(str) {
+    let commentarea = str.toLowerCase();
+  
+    return commentarea.includes('viagra') || commentarea.includes('xxx');
+  }
+  describe("checkSpam", function() {
+    it('считает спамом "buy ViAgRA now"', function() {
+      assert.isTrue(checkSpam('buy ViAgRA now'));
+    });
+  
+    it('считает спамом "free xxxxx"', function() {
+      assert.isTrue(checkSpam('free xxxxx'));
+    });
+  
+    it('не считает спамом "innocent rabbit"', function() {
+      assert.isFalse(checkSpam('innocent rabbit'));
+    });
+  });
