6a989e548692a8b1e37b6cdb838f7cb23cbfa9af
Oksanaka848 <okmitka@mail.ru>
2022-11-15 21:38:16 +0300

    ch

--- spam.js
+++ spam.js
@@ -29,14 +29,17 @@ let button = document.querySelector('[data-action="add"]');
 let commentList = document.getElementById('comment-list');
 
 button.addEventListener('click', () => {
-  let commentarea = document.getElementById('comment').value;
-  
+ 
+
   commentList.innerHTML+=`<div>${commentarea}</div><br>`;
   
   if (commentarea.includes("viagra")){
     commentList.innerHTML=`<div>${'***'}</div><br>`;}
-    else if (commentarea.includes("xxx")){
+    
+     if (commentarea.includes("xxx")){
       commentList.innerHTML=`<div>${'***'}</div><br>`;}  
+     
+
   });
 
 
